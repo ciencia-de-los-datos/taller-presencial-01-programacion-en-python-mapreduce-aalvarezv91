7a956bc90ef832d4cd9bedfb9253693a9597be56
Anderson Alvarez Vásquez <aalvarezv@unal.edu.co>
2024-02-23 20:31:48 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -49,7 +49,7 @@ def mapper(sequence):
         for word in words:
             word = word.replace(",", "")
             word = word.replace(".", "")
-            word = Word.lower()
+            word = word.lower()
             new_sequence.append((word, 1))
     return new_sequence
 
